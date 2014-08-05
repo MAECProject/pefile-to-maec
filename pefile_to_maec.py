c7f1150551ce00726411dfec81e123e885a68bfa
Charlie Hanner <channer@mitre.org>
2014-08-05 11:10:56 -0400

    [pe2m] Initialized pefile_to_maec.py for updating

--- pefile_to_maec.py
+++ pefile_to_maec.py
@@ -1,75 +1,38 @@
 # -*- coding: Latin-1 -*-
 # Copyright (c) 2014, The MITRE Corporation. All rights reserved.
 
-__version__ = 0.1
+__version__ = '0.1.0a1'
+
 import argparse
 import sys
 import os
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-import pefile
+try:
+    import pefile
+except ImportError:
+    print 'Unable to import pefile'
+    try:
+        sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+        import pefile
+    except Exception as err:
+        print 'Unable to import pefile from parent directory.\n'
+        print 'Please ensure that pefile resides in the current directory or \
+                install pefile.\n'
+        print 'ERROR MSG => %s' % err.message
+        sys.exit(-1)
 
-from mappings.image_dos_header import IMAGE_DOS_HEADER_MAPPINGS
-from mappings.image_file_header import IMAGE_FILE_HEADER_MAPPINGS
-from mappings.image_nt_headers import IMAGE_NT_HEADERS_MAPPINGS
-from mappings.image_optional_header import IMAGE_OPTIONAL_HEADER32_MAPPINGS
-
-class pefile_parser(object):
-    '''Parses a pefile.PE object.
-
-        Methods:
-            process_pefile:     TODO
-
-    '''
-    def __init__(self, pe):
-        self.pe = pe
-        self.headers = {}
-        self.process_pefile()
-
-    def set_dictionary_value(self, dictionary, key, value):
-        dictionary[key] = value
-
-    def perform_mappings(self, struct_inst, object_mapping_dict):
-        output_dict = {}
-        for key, value in struct_inst.dump_dict().items():
-            if key in object_mapping_dict:
-                if isinstance(object_mapping_dict[key], str):
-                    self.set_dictionary_value(output_dict, object_mapping_dict[key], str(getattr(struct_inst, key)))
-                elif isinstance(object_mapping_dict[key], dict):
-                    mapping_dict = object_mapping_dict[key]
-                    # Handle any fields from which the value must be extracted
-                    if 'Value' in mapping_dict:
-                        child_value = mapping_dict['Value']
-                        if child_value:
-                            self.set_dictionary_value(output_dict, mapping_dict['mapping'], str(child_value.strip()))
-
-        return output_dict
-
-    def process_headers(self):
-        pe_file_dictionary = {'xsi:type':'WindowsExecutableFileObjectType'}
-        pe_file_dictionary['headers'] = {}
-
-        pe_file_dictionary['headers']['image_dos_header'] = self.perform_mappings(self.pe.DOS_HEADER, IMAGE_DOS_HEADER_MAPPINGS)
-        pe_file_dictionary['headers']['image_file_header'] = self.perform_mappings(self.pe.FILE_HEADER, IMAGE_FILE_HEADER_MAPPINGS)
-        pe_file_dictionary['headers']['image_nt_header'] = self.perform_mappings(self.pe.NT_HEADERS, IMAGE_NT_HEADERS_MAPPINGS)
-        pe_file_dictionary['headers']['image_optional_header'] = self.perform_mappings(self.pe.OPTIONAL_HEADER, IMAGE_OPTIONAL_HEADER32_MAPPINGS)
-
-        return pe_file_dictionary
-
-    def process_pefile(self):
-        from pprint import pprint
-        pe_dict = self.process_headers()
-        pprint(pe_dict)
-        
 if __name__ == '__main__':
 
     # Setup the argument parser
-    parser = argparse.ArgumentParser(description="Ero Carerra's Pefile module to MAEC Translator v" + str(__version__))
+    parser = argparse.ArgumentParser(description="Ero Carrera's Pefile module to MITRE MAEC Translator v" + __version__)
     parser.add_argument("input", help="the name of the input portable executable file to translate to MAEC")
     parser.add_argument("output", help="the name of the MAEC XML to which the output will be written")
     args = parser.parse_args()
 
     # Open the input file and instantiate pefile.PE
-    pe = pefile.PE(args.input, fast_load=True)
-    # Instantiate the Pefile Parser and perform the parsing
-    parser = pefile_parser(pe)
-
+    try:
+        pe = pefile.PE(args.input, fast_load=True)
+    except pefile.PEFormatError as err:
+        print err.message
+        sys.exit(-1)
+    # Instantiate the pefile parser and parse the pefile object
+    parser = PefileParser(pe)
