16330627cddbb2489f0f619d7997fe78b0765b07
Charlie Hanner <channer@mitre.org>
2014-08-22 00:41:59 -0400

    Issue #6 - Modifies quasi-xpath

--- mappings/image_sections.py
+++ mappings/image_sections.py
@@ -3,13 +3,13 @@
 # See License.txt for complete terms.
 
 # IMAGE_SECTION_HEADER -> CybOX Windows Executable File Object Section Header Struct mappings
-IMAGE_SECTION_HEADER_MAPPINGS = {'Name':'sections/section_header/name',
-                                 'Misc_VirtualSize':'section/section_header/virtual_size',
-                                 'VirtualAddress':'sections/section_header/virtual_address',
-                                 'SizeOfRawData':'sections/section_header/size_of_raw_data',
-                                 'PointerToRawData':'sections/section_header/pointer_to_raw_data',
-                                 'PointerToRelocations':'sections/section_header/pointer_to_relocations',
-                                 'PointerToLinenumbers':'sections/section_header/pointer_to_linenumbers',
-                                 'NumberOfRelocations':'sections/section_header/number_of_relocations',
-                                 'NumberOfLinenumbers':'sections/section_header/number_of_linenumbers',
-                                 'Characteristics':'sections/section_header/characteristics'}
+IMAGE_SECTION_HEADER_MAPPINGS = {'Name':'section_header/name',
+                                 'Misc_VirtualSize':'section_header/virtual_size',
+                                 'VirtualAddress':'section_header/virtual_address',
+                                 'SizeOfRawData':'section_header/size_of_raw_data',
+                                 'PointerToRawData':'section_header/pointer_to_raw_data',
+                                 'PointerToRelocations':'section_header/pointer_to_relocations',
+                                 'PointerToLinenumbers':'section_header/pointer_to_linenumbers',
+                                 'NumberOfRelocations':'section_header/number_of_relocations',
+                                 'NumberOfLinenumbers':'section_header/number_of_linenumbers',
+                                 'Characteristics':'section_header/characteristics'}
